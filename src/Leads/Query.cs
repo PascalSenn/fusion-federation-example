4faa10afc8de913035fd4f4d2395d65b2073c2cb
Pascal Senn <senn.pasc@gmail.com>
2023-10-26 10:45:10 +0200

    Update

--- src/Leads/Query.cs
+++ src/Leads/Query.cs
@@ -5,7 +5,28 @@ namespace Leads;
 
 public class Query
 {
+    /// <summary>
+    /// If the Get{NameOfType}By{FieldId} does not match, you have to specify it in the
+    /// schema.extension.graphql file
+    /// </summary>
+    /// <param name="leadId"></param>
+    /// <returns></returns>
     public ConversationModel GetConversationByLeadId(Guid leadId) => new ConversationModel()
+    {
+        Lead = new LeadModel()
+        {
+            Id = leadId,
+            Name = "John Doe",
+        }
+    };
+
+    /// <summary>
+    /// If Get{NameOfType}By{FieldId} matches the Type and the Field it will be used as the
+    /// resolver.
+    /// </summary>
+    /// <param name="leadId"></param>
+    /// <returns></returns>
+    public ConversationModel GetConversationModelByLeadId(Guid leadId) => new ConversationModel()
     {
         Lead = new LeadModel()
         {
